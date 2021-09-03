89dba0ed02f2a1edf954d3d81d1198823ee079c2
SumedhBengale <ssbengale@gmail.com>
2021-09-03 17:35:36 +0530

    Added a bunch of stuff to main.js Mainly the function to change user's name.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,27 @@
-let myImage= document.querySelector("img");
-
-myImage.onclick= function(){
-
+function changeimage(){
     let src= myImage.getAttribute("src");
     if(src==="images/aot.jpg")
     {
         myImage.setAttribute('src','images/aot2.jpg');
     }
     else{
-
         myImage.setAttribute('src','images/aot.jpg');
     }
 }
+
+function setusername(){
+    let myName=prompt("Please Enter your name-");
+    myHeading.textContent = 'Welcome, ' + myName;
+    if(!localStorage.getItem('name')) {
+        setUserName();
+      } else {
+        let myName = localStorage.getItem('name');
+      }
+}
+
+let myImage= document.querySelector("img");
+
+myImage.onclick= function(){
+    changeimage();
+}
+let myHeading=document.querySelector('h2');
